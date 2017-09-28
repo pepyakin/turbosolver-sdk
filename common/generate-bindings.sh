d4f73c453a7cdceaaa5fb38547ef4cad9fb7c826
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-09-28 20:45:59 +0300

    Check if tools exists.

--- common/generate-bindings.sh
+++ common/generate-bindings.sh
@@ -9,6 +9,16 @@ if [ -z "$JAVA_SCHEMA_PATH" ]; then
     exit 1
 fi
 
+if ! type "capnpc-java" &>/dev/null; then
+    echo "capnpc-java isn't installed. See https://dwrensha.github.io/capnproto-java/index.html#installation"
+    exit 1
+fi
+
+if ! type "capnpc-rust" &>/dev/null; then
+    echo "capnpc-rust isn't installed. Try 'cargo install capnpc'."
+    exit 1
+fi
+
 capnpc \
     --import-path=$JAVA_SCHEMA_PATH \
     --output=java:android-demo/app/src/main/java/me/pepyakin/turbosolver/capnp \
