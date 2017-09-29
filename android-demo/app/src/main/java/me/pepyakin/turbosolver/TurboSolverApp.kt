7c1e3540e313060a035663d4bd697c2a07128778
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-09-29 22:34:56 +0300

    Clean.

--- android-demo/app/src/main/java/me/pepyakin/turbosolver/TurboSolverApp.kt
+++ android-demo/app/src/main/java/me/pepyakin/turbosolver/TurboSolverApp.kt
@@ -1,15 +1,13 @@
 package me.pepyakin.turbosolver
 
 import android.app.Application
-import dalvik.system.DexClassLoader
-import dalvik.system.DexFile
 
 class TurboSolverApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
         System.setProperty(
-                "dexmaker.dexcache",
-                getCacheDir().getPath())
+                "jnrffi.dexcache",
+                cacheDir.path)
     }
 }
